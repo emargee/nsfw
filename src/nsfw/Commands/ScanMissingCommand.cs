4482ad2be478b54967f95021244b3a33c13ac311
emargee <221695+emargee@users.noreply.github.com>
2024-02-03 15:19:56 +0000

    chore: missing build attributes

--- src/nsfw/Commands/ScanMissingCommand.cs
+++ src/nsfw/Commands/ScanMissingCommand.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using Nsfw.Nsp;
+﻿using Nsfw.Nsp;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.Spectre;
@@ -123,41 +122,5 @@ public class ScanMissingCommand : AsyncCommand<ScanMissingSettings>
         AnsiConsole.Write(new Rule());
 
         return 0;
-    }
-}
-
-public class ScanMissingSettings : CommandSettings
-{
-    [CommandOption("--titledb <FILE>")]
-    [Description("Path to titledb.db file.")]
-    [DefaultValue("./titledb/titledb.db")]
-    public string TitleDbFile { get; set; } = string.Empty;
-    
-    [CommandOption("-a|--all")]
-    [Description("Show all (matched + no updates)")]
-    public bool ShowAll { get; set; }
-    
-    [CommandArgument(0, "<NSP_DIR>")]
-    [Description("Path to NSP directory to scan.")]
-    public string ScanDir { get; set; } = string.Empty;
-
-    public override ValidationResult Validate()
-    {
-        if(ScanDir.StartsWith('~'))
-        {
-            ScanDir = ScanDir.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-        }
-        
-        if(TitleDbFile.StartsWith('~'))
-        {
-            TitleDbFile = TitleDbFile.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-        }
-        
-        if (string.IsNullOrWhiteSpace(ScanDir))
-        {
-            return ValidationResult.Error("Scan directory is required.");
-        }
-
-        return base.Validate();
     }
 }
---
+++ src/nsfw/Commands/ScanMissingSettings.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Nsfw.Commands;
+
+public class ScanMissingSettings : CommandSettings
+{
+    [CommandOption("--titledb <FILE>")]
+    [Description("Path to titledb.db file.")]
+    [DefaultValue("./titledb/titledb.db")]
+    public string TitleDbFile { get; set; } = string.Empty;
+    
+    [CommandOption("-a|--all")]
+    [Description("Show all (matched + no updates)")]
+    public bool ShowAll { get; set; }
+    
+    [CommandArgument(0, "<NSP_DIR>")]
+    [Description("Path to NSP directory to scan.")]
+    public string ScanDir { get; set; } = string.Empty;
+
+    public override ValidationResult Validate()
+    {
+        if(ScanDir.StartsWith('~'))
+        {
+            ScanDir = ScanDir.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        }
+        
+        if(TitleDbFile.StartsWith('~'))
+        {
+            TitleDbFile = TitleDbFile.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        }
+        
+        if (string.IsNullOrWhiteSpace(ScanDir))
+        {
+            return ValidationResult.Error("Scan directory is required.");
+        }
+
+        return base.Validate();
+    }
+}
